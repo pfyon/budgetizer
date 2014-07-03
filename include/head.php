a9a64aee21b0c7f3bddc0ee28a9aabf57c258ddd
pfyon <peter.fyon@gmail.com>
2014-07-03 11:41:51 -0400

    Moved header into this file

---
+++ include/head.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '
+<title>Budgetizer</title>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/redmond/jquery-ui.css" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>';
+
+?>
