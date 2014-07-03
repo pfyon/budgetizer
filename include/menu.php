8ce2dde5662769efe61cf3790c5d59d61faaad55
pfyon <peter.fyon@gmail.com>
2014-07-03 11:38:46 -0400

    Added to provide a standard set of navigation links to the top of the page

---
+++ include/menu.php
@@ -0,0 +1,8 @@
+<?php
+echo '
+<div id="main_navigation_menu">
+	<div class="navigation_link"><a href="index.php">Home</a></div>
+	<div class="navigation_link"><a href="upload.php">Upload a Transaction File</a></div>
+	<div class="navigation_link"><a href="stats.php">Statistics</a></div>
+</div>';
+?>
