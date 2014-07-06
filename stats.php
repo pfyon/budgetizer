82ff361dd99705626502282fe871db2aa3137f15
pfyon <peter.fyon@gmail.com>
2014-07-06 18:46:26 -0400

    Now enforces authentication. Now allows users to add tags to track for statistics. Increased the usefulness of the reported statistics.

--- stats.php
+++ stats.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
 require_once("config.php");
+
+//This include basically checks that they're logged in and redirects them to login.php if they're not
+require_once("include/auth.php");
+
 require_once("class/Transactions.php");
+require_once("class/TagStats.php");
 
 echo '<html><head>';
 require_once("include/head.php");
@@ -9,25 +14,51 @@ echo '</head><body>';
 
 require_once("include/menu.php");
 
+if(isset($_POST['statistics_submit']))
+{
+	$tag = trim($_POST['statistics_tag']);
+	if($tag != '')
+	{
+		TagStats::addTag($_SESSION['auth']['user_id'], $tag, $db_cnx);
+	}
+}
 
-$tags = array('food', 'alcohol', 'coffee');
 
-$cnx = pg_connect("host='" . $host . "' dbname='" . $dbname . "' user='" . $user . "' password='" . $password . "'");
+$tags = TagStats::getTagsByUserId($_SESSION['auth']['user_id'], $db_cnx);
+
+echo '
+<div class="container">
+	<form name="statistics_addtag" method="POST">
+		<fieldset>
+			<legend>Add Tag to Statistics Tracking</legend>
+			<div class="form_div">Tag: <input type="text" name="statistics_tag" value="" /></div>
+			<div class="form_div"><input type="submit" name="statistics_submit" value="Add Tag" /></div>
+		</fieldset>
+	</form>
+</div>';
+
+
 $first_of_month = (new DateTime("first day of last month"))->format('m/d/Y');
 $last_of_month = (new DateTime("last day of last month"))->format('m/d/Y');
 
-$total_credits_last_month = Transactions::getCreditsByDateRange($first_of_month, $last_of_month, $cnx) / 100;
-$total_debits_last_month = Transactions::getDebitsByDateRange($first_of_month, $last_of_month, $cnx) / 100;
-$total_last_month_food = Transactions::getTotalByTagsAndDateRange($tags, $first_of_month, $last_of_month, $cnx) / 100;
-$average_last_year_food = Transactions::getMonthlyAverageOverYearByTags($tags, $cnx) / 100;
+echo '<div class="container"><table class="column_100">
+	<tr>
+		<th></th>
+		<th>Total Last Month</th>
+		<th>Monthly Average</th>
+</tr>';
+foreach($tags as $tag)
+{
+	$total_last_month = Transactions::getTotalByTagsAndDateRange(array($tag), $first_of_month, $last_of_month, $db_cnx) / 100;
+	$average_last_year = Transactions::getMonthlyAverageOverYearByTags(array($tag), $db_cnx) / 100;
+	echo '<tr>
+		<td>' . $tag . '</td>
+		<td>' . sprintf('%1.2f', $total_last_month) . '</td>
+		<td>' . sprintf('%1.2f', $average_last_year) . '</td>
+	</tr>';
+}
 
-echo "<div>";
-echo "<div>Total credits last month:</div><div>" . $total_credits_last_month . "</div>";
-echo "<div>Total debits last month:</div><div>" . $total_debits_last_month . "</div>";
-echo "<div>Total spent on food (TODO: make this any tag) last month:</div><div>" . sprintf('%1.2f', $total_last_month_food) . "</div>";
-echo "<div>Average spent on food (TODO: make this any tag) every month over last year:</div><div>" . sprintf('%1.2f', $average_last_year_food) . "</div>";
-echo "</div>";
-
+echo '</table></div>';
 
 echo '</body></html>';
 ?>
