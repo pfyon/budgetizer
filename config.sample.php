d3c735ee629a1c3a6cd3cd2dce6fe1beab6672ac
pfyon <peter.fyon@gmail.com>
2014-07-07 09:59:31 -0400

    Added the db connection creation.

--- config.sample.php
+++ config.sample.php
@@ -3,4 +3,6 @@ $host = 'localhost';
 $dbname = 'budgetizer';
 $user = 'budget';
 $password = 'budgetpassword';
+
+$db_cnx = pg_connect("host='" . $host . "' dbname='" . $dbname . "' user='" . $user . "' password='" . $password . "'");
 ?>
