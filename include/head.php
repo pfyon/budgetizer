9876bdc5e7e1d453baa90c22b5ae9848119f0fcc
pfyon <peter.fyon@gmail.com>
2014-07-07 11:21:57 -0400

    Fixed jqueryui to use https version.

--- include/head.php
+++ include/head.php
@@ -3,7 +3,7 @@
 echo '
 <title>Budgetizer</title>
 <link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/redmond/jquery-ui.css" />
+<link rel="stylesheet" type="text/css" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/redmond/jquery-ui.css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js" type="text/javascript"></script>';
 
