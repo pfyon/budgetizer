0cf56e5cd28f729c98d39dc231cc035485a6275f
pfyon <peter.fyon@gmail.com>
2014-07-07 09:54:12 -0400

    Added getTagIdByLabel()

--- class/Tags.php
+++ class/Tags.php
@@ -18,5 +18,17 @@ class Tags
 		}
 		return $tags;
 	}
+
+	public static function getTagIdByLabel($user_id, $label, $cnx)
+	{
+		$result = pg_query($cnx, "SELECT id FROM tag_labels WHERE owner = " . pg_escape_string($user_id) . " AND label = '" . pg_escape_string($label) . "' LIMIT 1");
+		if($result && pg_num_rows($result) > 0)
+		{
+			$row = pg_fetch_assoc($result, 0);
+			return $row['id'];
+		}
+
+		return false;
+	}
 }
 ?>
