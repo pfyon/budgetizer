013821c57a3007ef30b724777928de804d033aac
pfyon <peter.fyon@gmail.com>
2014-07-07 09:58:01 -0400

    Added a logout link.

--- include/menu.php
+++ include/menu.php
@@ -1,8 +1,9 @@
 <?php
 echo '
-<div id="main_navigation_menu">
+<div id="main_navigation_menu" class="container">
 	<div class="navigation_link"><a href="index.php">Home</a></div>
 	<div class="navigation_link"><a href="upload.php">Upload a Transaction File</a></div>
 	<div class="navigation_link"><a href="stats.php">Statistics</a></div>
+	<div class="navigation_link"><a href="logout.php">Log Out</a></div>
 </div>';
 ?>
