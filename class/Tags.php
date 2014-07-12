a6d46882de042194443bb198b1b74c4c2b872218
pfyon <peter.fyon@gmail.com>
2014-07-11 22:35:27 -0400

    Renamed getTagIdByLabel() to getIdByLabel(). Added addLabel() and removeLabel(). Added support for owner.

--- class/Tags.php
+++ class/Tags.php
@@ -1,12 +1,12 @@
 <?php
 class Tags
 {
-	public static function getAll($cnx)
+	public static function getAll($user_id, $cnx)
 	{
 		$tags = array();
 		if($cnx)
 		{
-			$result = pg_query($cnx, "SELECT label FROM tag_labels ORDER BY label");
+			$result = pg_query($cnx, "SELECT label FROM tag_labels WHERE owner = " . $user_id . " ORDER BY label");
 
 			if($result !== false)
 			{
@@ -19,15 +19,41 @@ class Tags
 		return $tags;
 	}
 
-	public static function getTagIdByLabel($user_id, $label, $cnx)
+	public static function addLabel($user_id, $label, $cnx)
 	{
-		$result = pg_query($cnx, "SELECT id FROM tag_labels WHERE owner = " . pg_escape_string($user_id) . " AND label = '" . pg_escape_string($label) . "' LIMIT 1");
-		if($result && pg_num_rows($result) > 0)
+		$result = pg_query($cnx, "INSERT INTO tag_labels (label, owner) VALUES ('" . pg_escape_string($label) . "', " . $user_id . ") RETURNING id");
+		if(pg_num_rows($result) == 1)
 		{
 			$row = pg_fetch_assoc($result, 0);
 			return $row['id'];
 		}
 
+		return false;
+	}
+
+	public static function getIdByLabel($user_id, $label, $cnx)
+	{
+		$result = pg_query($cnx, "SELECT id FROM tag_labels WHERE label = '" . pg_escape_string($label) . "' AND owner = " . $user_id . " LIMIT 1");
+
+		if(pg_num_rows($result) == 1)
+		{
+			$row = pg_fetch_assoc($result, 0);
+
+			return $row['id'];
+		}
+
+		//Label doesn't exist
+		return false;
+	}
+
+	public static function removeLabel($owner, $label, $cnx)
+	{
+		$result = pg_query($cnx, "DELETE FROM tag_labels WHERE label = '" . pg_escape_string($label) . "' AND owner = " . $owner);
+
+		if($result)
+		{
+			return true;
+		}
 		return false;
 	}
 }
