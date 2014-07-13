6d59d7a95a09db100abd6fac717ff3b7613c0577
pfyon <peter.fyon@gmail.com>
2014-07-13 11:32:53 -0400

    Fixed include path for TagStats.php addTag() will now try to create the label if it doesn't exist. This is used by budget.php to allow a user to create a budget for a tag that doesn't exist yet. Added getBudgetByLabel(). Added updateBudgetByLabel(). Added getNumBudgetsByTag(). This is used when removing tags to ensure the tag doesn't get removed if there's a budget for it.

--- class/TagStats.php
+++ class/TagStats.php
@@ -1,11 +1,17 @@
 <?php
-require_once("class/Tags.php");
+require_once("../class/Tags.php");
 
 class TagStats
 {
 	public static function addTag($user_id, $label, $cnx)
 	{
-		$tagid = Tags::getTagIdByLabel($user_id, $label, $cnx);
+		$tagid = Tags::getIdByLabel($user_id, $label, $cnx);
+
+		if(!$tagid)
+		{
+			//Tag doesn't exist, we need to add it
+			$tagid = Tags::addLabel($user_id, $label, $cnx);
+		}
 
 		if($tagid)
 		{
@@ -33,6 +39,47 @@ class TagStats
 		}
 
 		return $tags;
+	}
+
+	public static function getBudgetByLabel($user_id, $label, $cnx)
+	{
+		$tagid = Tags::getIdByLabel($user_id, $label, $cnx);
+		$result = pg_query($cnx, "SELECT budget FROM tag_stats WHERE user_id = " . $user_id . " AND tag_label_id = " . $tagid . " LIMIT 1");
+
+		if($result)
+		{
+			$row = pg_fetch_assoc($result, 0);
+			return $row['budget'];
+		}
+		return NULL;
+	}
+
+	public static function updateBudgetByLabel($user_id, $label, $budget, $cnx)
+	{
+		$tagid = Tags::getIdByLabel($user_id, $label, $cnx);
+
+		if($tagid)
+		{
+			$result = pg_query($cnx, "UPDATE tag_stats SET budget = " . $budget . " WHERE tag_label_id = " . $tagid . " AND user_id = " . $user_id);
+			if($result)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static function getNumBudgetsByTag($user_id, $tag, $cnx)
+	{
+		$result = pg_query($cnx, "SELECT COUNT(s.id) FROM tag_stats s JOIN tag_labels l ON s.tag_label_id = l.id WHERE l.label = '" . pg_escape_string($tag) . "' AND l.owner = " . $user_id . " AND s.user_id = " . $user_id);
+		
+		if($result)
+		{
+			$row = pg_fetch_assoc($result, 0);
+			return $row['count'];
+		}
+
+		return false;
 	}
 }
 ?>
